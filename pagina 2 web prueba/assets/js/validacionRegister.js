650f8fe86f5d8c1b08d2420234f8897ddab6d887
Flambax1 <79771598+Flambax1@users.noreply.github.com>
2024-05-17 00:47:57 -0400

    Add files via upload

---
+++ pagina 2 web prueba/assets/js/validacionRegister.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $("#bEnviar2").click(function (event) {
+        // Prevenir el envío del formulario
+        event.preventDefault();
+
+        // Limpiar mensajes de error anteriores
+        $(".error-message").hide();
+
+        var nombre = $("#name").val();
+        var correo = $("#email").val();
+        var contraseña = $("#pass").val();
+        var isValid = true;
+
+        if (nombre == "") {
+            $("#nameError").text("El campo nombre es obligatorio").show();
+            isValid = false;
+        }
+        if (!correo.includes('@')){
+            $("#emailError").text("Por favor ingrese un correo válido").show();
+            isValid = false;
+        }
+        if (contraseña == "") {
+            $("#passwordError").text("El campo contraseña es obligatorio").show();
+            isValid = false;
+        }
+
+        // Si todos los campos son válidos, envía el formulario
+        if (isValid) {
+            $("#registrationForm").submit();
+        }
+    });
+});
